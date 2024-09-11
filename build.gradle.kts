16f7f1ab7ec5514cbd0913c0c1a78f3a4dae9702
RuslanKharkevych <kharkevychruslan@gmail.com>
2024-09-12 01:59:30 +0300

    Bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ buildscript {
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kapt) apply false
     alias(libs.plugins.kotlinAndroid) apply false
 }
