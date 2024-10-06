443bbd9453d28feafd50afdfe01b2c1bef8268bb
RuslanKharkevych <kharkevychruslan@gmail.com>
2024-10-06 13:46:17 +0300

    Fix issue with R8

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -15,16 +15,6 @@ android {
         consumerProguardFiles("consumer-rules.pro")
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-
     buildFeatures {
         buildConfig = true
         compose = true
